9f0638ee7d25a8727a8119dcc3b569fd529ce96f
Albert <alberthyantonio@yahoo.com.br>
2020-03-19 10:36:30 -0300

    Initial Commit

---
+++ ClickBusTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClickBusTest
+//
+//  Created by Albert Oliveira on 19/03/20.
+//  Copyright © 2020 Albert Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
